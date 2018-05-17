b4246e64d87e8ae62b919f73a16bd48af882a69b
David Van Der Beek <earlynovrock@gmail.com>
2018-05-16 20:59:49 -0500

    handle offline development gracefully

--- lib/segment/scopes/controller_extension.rb
+++ lib/segment/scopes/controller_extension.rb
@@ -3,11 +3,11 @@ module Segment
     module ControllerExtension
       private
 
-        def segment(view)
+        def segment(view, model)
           @scopes = Scope.for_view(view)
           @scope  = Scope.for_param(@scopes, params[:scope])
           query   = @scope.build_query(params.to_unsafe_h.fetch(:q, {}), send(Segment::Scopes.current_user_method))
-          @q = @scope.model_name.classify.safe_constantize.ransack(query)
+          @q = model.ransack(query)
         end
     end
   end
